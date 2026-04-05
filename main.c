b725af1f40f005f5b729525d5d324b59be951a25
MiguelTAR <mtarazona4489@conestogac.on.ca>
2026-04-04 20:49:49 -0400

    Interactive interfaces for the user to input values for create_task and delete_task functions.

--- main.c
+++ main.c
@@ -6,6 +6,56 @@
 static void read(char* buffer, size_t size) {
 	if (fgets(buffer, (int)size, stdin) != NULL) {
 		size_t len = strlen(buffer);
+		if (len > 0 && buffer[len - 1] == '\n') {
+			buffer[len - 1] = '\0';
+		}
 	}
 
+}
+
+
+void create_interact_task(Task task[], int* task_count, int* next_id)
+{
+	char description[200];
+	int priority_int;
+
+	printf("Please Enter Task Description: ");
+	read(description, sizeof(description));
+
+
+	printf("Please Enter Priority (1 = Highest, 5 = Lowest): ");
+	scanf("%d", &priority_int);
+	while (getchar() != '/n');
+
+	if (priority_int < 1 || priority_int > 5) {
+		printf("Invalid Priority.\n");
+		return;
+	}
+	Task t = create_task(*next_id, description, (Priority)priority_int);
+	
+	if (add_task(task, task_count, t) == 0) {
+		printf("Task created with ID %d.\n", *next_id);
+		(*next_id)++;
+	}
+	else {
+		printf("Task List full.\n");
+	}
+
+}
+
+void delete_interact_task(Task task[], int* task_count) {
+
+	int priority_int;
+
+	printf("enter priority (1-5) of tasks to delete: ");
+	scanf("%d", &priority_int);
+	while (getchar() != '\n');
+
+	if (priority_int < 1 || priority_int > 5) {
+		printf("Invalid Priority.\n");
+		return;
+	}
+
+	int deleted = delete_task(task, task_count, (Priority)priority_int);
+	printf("Deleted %d tasks with priority %d.\n", deleted, priority_int);
 }
