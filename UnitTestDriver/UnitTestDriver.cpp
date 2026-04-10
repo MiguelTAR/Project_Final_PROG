2d93f223349da25c057c8b5e2bef817955792cb5
MiguelTAR <mtarazona4489@conestogac.on.ca>
2026-04-10 14:46:23 -0400

    Test Cases for Section 1

--- UnitTestDriver/UnitTestDriver.cpp
+++ UnitTestDriver/UnitTestDriver.cpp
@@ -1,6 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include "../main.h"
+#include "../Task_Manager.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -12,8 +12,43 @@ namespace UnitTestDriver
 		
 		TEST_METHOD(TestMethod1)
 		{
-			Assert::AreEqual(dummyFunction(3, 5), 8);
-			Assert::AreEqual(dummyFunction(2, 4), 6);
+			Task t = create_task(1, "Test Task", priority_1);
+			Assert::AreEqual(1, t.id);
+			Assert::AreEqual("Test Task", t.description);
+			Assert::AreEqual((int)priority_1, (int)t.priority);
+			Assert::AreEqual((int)status_not_started, (int)t.status);
+
 		}
+		TEST_METHOD(TestMethod2)
+		{
+			Task tasks[max_tasks];
+			int count = 0;
+			Task t = create_task(1, "Task 1", priority_2);
+
+			int result = add_task(tasks, &count, t);
+
+			Assert::AreEqual(0, result);
+			Assert::AreEqual(1, count);
+			Assert::AreEqual(1, tasks[0].id);
+			Assert::AreEqual("Task 1", tasks[0].description);
+			Assert::AreEqual((int)priority_2, (int)tasks[0].priority);
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			Task tasks[max_tasks];
+			int count = 0;
+
+			add_task(tasks, &count, create_task(1, "P1", priority_1));
+			add_task(tasks, &count, create_task(2, "P2", priority_2));
+			add_task(tasks, &count, create_task(3, "P3", priority_3));
+
+			int deleted = delete_task(tasks, &count, priority_1);
+
+			Assert::AreEqual(2, deleted);
+			Assert::AreEqual(1, count);
+			Assert::AreEqual(2, tasks[0].id);
+
+		}
+
 	};
 }
